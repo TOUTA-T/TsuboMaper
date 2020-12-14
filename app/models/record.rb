b59cf8579d2f779793fa74aac5eb6f4811fe439b
TOUTA-T <toutaby@aol.com>
2020-12-15 03:14:07 +0900

    Add record CRUD

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,10 @@
 class Record < ApplicationRecord
+  validates :person_in_charge,  presence: true, length: { maximum: 30 }
+  validates :comment, presence: true, length: { maximum: 200 }
+  validates :pain_a,  presence: true
+  # picture系は一旦presenceバリデーションなしで
+  mount_uploader :expect_picture, ImageUploader
+  mount_uploader :treatment_picture, ImageUploader
+  mount_uploader :storage_picture, ImageUploader
+  #できれば保存ファイルはそれぞれで分ける設定にしたい（低）
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'records/new'
   root'records#new'
-  resource :records
+  resources :records
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
 end
